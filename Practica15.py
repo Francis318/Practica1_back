6c069667a4f683a6dbf4ada536faa0c977faa7ee
Francis318 <enriquereyes32@hotmail.com>
2023-06-28 18:30:23 -0600

    Mejora de practica15

--- Practica15.py
+++ Practica15.py
@@ -1,8 +1,13 @@
-def validar(numero):
-  if numero.isdigit():
-    print("El strig ",numero," tiene numeros")
+def validar(letras):
+  repeticion=0
+  for iteracion in letra:
+    if iteracion.isdigit():
+      repeticion+=1
+  if repeticion>1:
+    print("El strig ",letras," tiene numeros")
   else:
-    print("El string ",numero," no tiene numeros")
+    print("El string ",letras," no tiene numeros")
 
 letra=input("Ingrese un string: ")
 validar(letra)
+
